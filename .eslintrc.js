5bbf993c56090792f449eb1368cc3e173dab7e5f
Simon Okutan <simon.spam16@gmail.com>
2024-12-11 22:04:21 +0100

    Refactor linter settings to resolve conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,79 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint',
+    'eslint-comments',
+    'promise',
+    'unicorn'
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:eslint-comments/recommended',
     'airbnb-base',
-    'airbnb-typescript/base'
+    'airbnb-typescript/base',
+    'prettier'
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    'no-prototype-builtins': 'off',
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    'import/prefer-default-export': 'off',
+    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    'react/destructuring-assignment': 'off',
+    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+    'react/jsx-filename-extension': 'off',
+    // Use function hoisting to improve code readability
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true }
+    ],
+    // Allow most functions to rely on type inference. If the function is exported, then `@typescript-eslint/explicit-module-boundary-types` will ensure it's typed.
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
     'class-methods-use-this': 'off',
     'implicit-arrow-linebreak': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true }
+    ],
+    // Common abbreviations are known and readable
+    'unicorn/prevent-abbreviations': 'off',
+    // Airbnb prefers forEach
+    'unicorn/no-array-for-each': 'off',
+    // No option to set states to null
+    'unicorn/no-null': 'off',
+    // Makes problems
+    'unicorn/expiring-todo-comments': 'off',
+    // It's not accurate in the monorepo style
+    'import/no-extraneous-dependencies': 'off',
+    // Disable this rule for the hooks folder
+    'unicorn/filename-case': 'off',
+    // Next uses default exports
+    'import/no-default-export': 'off'
   },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        // Allow CJS until ESM support improves
+        '@typescript-eslint/no-var-requires': 'off',
+        'unicorn/prefer-module': 'off'
+      }
+    }
+  ]
 };
