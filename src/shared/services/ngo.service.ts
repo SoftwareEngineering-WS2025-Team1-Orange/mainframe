95dcc249802e2bb98b8907876de81189fe16a666
Samuel Sacher <3.10@web.de>
2024-12-28 20:21:36 +0100

    a) Refactoring: Filtering always using filter interfaces b) Added favorization and donation in project/ngo interfaces

--- src/shared/services/ngo.service.ts
+++ src/shared/services/ngo.service.ts
@@ -1,18 +1,17 @@
-import {HttpException, Injectable} from '@nestjs/common';
-import {NGO, NGOScopeEnum, Prisma} from '@prisma/client';
-import {StatusCodes} from 'http-status-codes';
-import {randomBytes} from 'node:crypto';
+import { HttpException, Injectable } from '@nestjs/common';
+import { NGO, NGOScopeEnum, Prisma } from '@prisma/client';
+import { StatusCodes } from 'http-status-codes';
+import { randomBytes } from 'node:crypto';
 import * as argon2 from 'argon2';
-import {PrismaService} from '@/shared/prisma/prisma.service';
-import {CreateNgoDto} from '@/api-ngo/ngo/dto/ngo.dto';
-import {Pagination} from '@/utils/pagination/pagination.helper';
-import {NGOWithScope} from '@/api-ngo/auth/types';
-import {NgoFilter} from '@/shared/filters/ngo.filter.interface';
+import { PrismaService } from '@/shared/prisma/prisma.service';
+import { CreateNgoDto } from '@/api-ngo/ngo/dto/ngo.dto';
+import { Pagination } from '@/utils/pagination/pagination.helper';
+import { NGOWithScope } from '@/api-ngo/auth/types';
+import { NgoFilter } from '@/shared/filters/ngo.filter.interface';
 
 @Injectable()
 export class NgoService {
-  constructor(private prismaService: PrismaService) {
-  }
+  constructor(private prismaService: PrismaService) {}
 
   async findNgoById(id: number): Promise<NGO> {
     const ngo = await this.prismaService.nGO.findFirst({
@@ -40,15 +39,14 @@ export class NgoService {
     }: { ngos: NGOWithScope[]; pagination: Pagination } =
       await this.findFilteredNgos(filters);
 
-    const favorizedNgos =
-      await this.prismaService.nGO.findMany({
-        select: {
-          id: true,
-        },
-        where: {
-          favouritedByDonators: {some: {id: favourizedByDonatorId}},
-        },
-      })
+    const favorizedNgos = await this.prismaService.nGO.findMany({
+      select: {
+        id: true,
+      },
+      where: {
+        favouritedByDonators: { some: { id: favourizedByDonatorId } },
+      },
+    });
 
     const favorizedNgoIDs: Set<number> = new Set(
       favorizedNgos.map((ngo) => ngo.id),
@@ -58,7 +56,7 @@ export class NgoService {
       ...ngo,
       isFavorite: favorizedNgoIDs.has(ngo.id),
     }));
-    return {ngos: ngosWithIsFavorite, pagination};
+    return { ngos: ngosWithIsFavorite, pagination };
   }
 
   async findFilteredNgos(
@@ -66,47 +64,47 @@ export class NgoService {
   ): Promise<{ ngos: NGOWithScope[]; pagination: Pagination }> {
     const whereInputObject: Prisma.NGOWhereInput = {
       AND: [
-        filters.filterId ? {id: filters.filterId} : {},
+        filters.filterId ? { id: filters.filterId } : {},
         filters.filterName
-          ? {name: {contains: filters.filterName, mode: 'insensitive'}}
+          ? { name: { contains: filters.filterName, mode: 'insensitive' } }
           : {},
         filters.filterMail
-          ? {email: {contains: filters.filterMail, mode: 'insensitive'}}
+          ? { email: { contains: filters.filterMail, mode: 'insensitive' } }
           : {},
         filters.filterFavorizedByDonatorId
           ? {
-            favouritedByDonators: {
-              some: {id: filters.filterFavorizedByDonatorId},
-            },
-          }
+              favouritedByDonators: {
+                some: { id: filters.filterFavorizedByDonatorId },
+              },
+            }
           : {},
         filters.filterNotFavorizedByDonatorId
           ? {
-            favouritedByDonators: {
-              none: {id: filters.filterNotFavorizedByDonatorId},
-            },
-          }
+              favouritedByDonators: {
+                none: { id: filters.filterNotFavorizedByDonatorId },
+              },
+            }
           : {},
         filters.filterDonatedToByDonatorId
           ? {
-            donations: {
-              some: {donatorId: filters.filterDonatedToByDonatorId},
-            },
-          }
+              donations: {
+                some: { donatorId: filters.filterDonatedToByDonatorId },
+              },
+            }
           : {},
         filters.filterNotDonatedToByDonatorId
           ? {
-            donations: {
-              none: {donatorId: filters.filterNotDonatedToByDonatorId},
-            },
-          }
+              donations: {
+                none: { donatorId: filters.filterNotDonatedToByDonatorId },
+              },
+            }
           : {},
       ],
     };
 
     const numTotalResults = await this.prismaService.nGO.count();
     const numFilteredResults = await this.prismaService.nGO.count({
-      where: {...whereInputObject},
+      where: { ...whereInputObject },
     });
     const pagination = new Pagination(
       numTotalResults,
@@ -115,9 +113,9 @@ export class NgoService {
       filters.paginationPage,
     );
     const ngos = await this.prismaService.nGO.findMany({
-      where: {...whereInputObject},
+      where: { ...whereInputObject },
       ...pagination.constructPaginationQueryObject(),
-      orderBy: {[this.getSortField(filters.sortFor)]: filters.sortType},
+      orderBy: { [this.getSortField(filters.sortFor)]: filters.sortType },
       include: {
         scope: true,
       },
@@ -157,8 +155,8 @@ export class NgoService {
         salt,
         scope: {
           connectOrCreate: defaultRoles.map((scope) => ({
-            where: {name: scope},
-            create: {name: scope},
+            where: { name: scope },
+            create: { name: scope },
           })),
         },
       },
