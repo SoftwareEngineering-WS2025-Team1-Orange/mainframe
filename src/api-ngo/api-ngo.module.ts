abb40f69a48b921521f666bb868db32759a042ee
Simon Okutan <simon.spam16@gmail.com>
2025-01-02 18:42:56 +0100

    Add project to api-ngo module

--- src/api-ngo/api-ngo.module.ts
+++ src/api-ngo/api-ngo.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '@/api-ngo/auth/auth.module';
 import { NgoModule } from '@/api-ngo/ngo/ngo.module';
+import { ProjectModule } from '@/api-ngo/project/project.module';
 
 @Module({
-  imports: [AuthModule, NgoModule],
+  imports: [AuthModule, NgoModule, ProjectModule],
   controllers: [],
   providers: [],
 })
