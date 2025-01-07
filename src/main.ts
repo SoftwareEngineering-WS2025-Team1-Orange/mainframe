6a8ef5c3e995559b1bbd3d03b55b27e1ef9ebfa0
Simon Okutan <simon.spam16@gmail.com>
2025-01-07 16:32:04 +0100

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,20 @@ import AppModule from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
   });
-
+  app.enableCors();
   app.useWebSocketAdapter(new WsAdapter(app));
-
   app.use(cookieParser());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
