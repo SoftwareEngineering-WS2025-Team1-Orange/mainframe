d791dea1e8dc37d9051e10052b4419e0ff7d78b6
Samuel Sacher <3.10@web.de>
2025-01-11 12:17:13 +0100

    Trying to solve linter issues

--- src/shared/services/types/ProjectWithFavourite.ts
+++ src/shared/services/types/ProjectWithFavourite.ts
@@ -1,5 +1,8 @@
 import { ProjectWithPartialRelations } from './projectWithPartialRelations';
 
-export type ProjectWithFavourite = Omit<ProjectWithPartialRelations, 'is_favorite'> & {
+export type ProjectWithFavourite = Omit<
+  ProjectWithPartialRelations,
+  'is_favorite'
+> & {
   is_favorite: boolean;
 };
--- src/shared/services/types/ProjectWithPartialRelations.ts
+++ src/shared/services/types/ProjectWithPartialRelations.ts
@@ -1,7 +1,11 @@
 import { NGO, Project, Donation, Donator } from '@prisma/client';
 
+type PartialNgo = Pick<NGO, 'id' | 'name'>;
+type PartialDonation = Pick<Donation, 'id' | 'amount' | 'createdAt'>;
+type PartialDonator = Pick<Donator, 'id'>;
+
 export type ProjectWithPartialRelations = Project & {
-  ngo?: Pick<NGO, 'id' | 'name'>;
-  donations?: Pick<Donation, 'id' | 'amount' | 'createdAt'>[];
-  FavouritedByDonators?: Pick<Donator, 'id'>[];
+  ngo?: PartialNgo;
+  donations?: PartialDonation[];
+  FavouritedByDonators?: PartialDonator[];
 };
