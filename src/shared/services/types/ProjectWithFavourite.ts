bc31b7962c3a239a6672c6ce34e0473548b60b8a
Samuel Sacher <3.10@web.de>
2025-01-11 13:38:16 +0100

    removed partial type for project

--- src/shared/services/types/ProjectWithFavourite.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectWithPartialRelations } from './projectWithPartialRelations';
-
-export type ProjectWithFavourite = Omit<
-  ProjectWithPartialRelations,
-  'is_favorite'
-> & {
-  is_favorite: boolean;
-};
