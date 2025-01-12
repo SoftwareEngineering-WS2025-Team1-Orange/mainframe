3572f966d30cfab0e5b6bca283b884d5604e61a4
Simon Okutan <simon.spam16@gmail.com>
2025-01-12 21:22:31 +0100

    Add endpoint annotation

--- src/api-ngo/donation/donation.controller.ts
+++ src/api-ngo/donation/donation.controller.ts
@@ -1,6 +1,7 @@
 import {
   ClassSerializerInterceptor,
   Controller,
+  Get,
   Param,
   ParseIntPipe,
   Query,
@@ -30,6 +31,7 @@ export class DonationController {
   @Version('1')
   @UseInterceptors(ClassSerializerInterceptor)
   @SerializeOptions({ type: ReturnPaginatedDonationsDto })
+  @Get('/')
   @UseGuards(NGOAccessTokenGuard, ScopesGuard)
   @Scopes(NGOScopeEnum.READ_DONATION)
   getDonations(
