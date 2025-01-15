e7a250adf465421e18438a7d86b57874349e367d
Samuel Sacher <3.10@web.de>
2025-01-15 21:32:52 +0100

    fundraising_current is now calculated

---
+++ prisma/migrations/20250115200332_removed_fundraising_current/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `fundraising_current` on the `Project` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" DROP COLUMN "fundraising_current";
